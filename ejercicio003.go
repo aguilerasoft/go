2a17e0bbb2420d08864b9d21f2380054643472c7
Cristian Aguilera <aguilerasoft@github.com>
2021-03-17 22:52:46 -0400

    1

--- ejercicio003.go
+++ ejercicio003.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+    var lado int
+    var superficie int
 
-
-	
+    fmt.Print("Ingrese el valor del lado del cuadrado:")
+    fmt.Scan(&lado)
+    superficie = lado * lado
+    fmt.Println("La superficie del cuadrado es:",superficie)
 }
